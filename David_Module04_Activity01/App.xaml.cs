f2ff002d9501d5a56ab048c14d8fd2f6a56aaceb
Kaila David <david.arizakaila@auf.edu.ph>
2021-09-15 11:55:30 +0800

    Add project files.

---
+++ David_Module04_Activity01/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace David_Module04_Activity01
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
